d0e55e94e9323771cb45720c24a8057080b912ac
AsgerD1000 <AsgerDalmeijer@gmail.com>
2024-11-24 20:25:11 +0100

    updated time to nova mono in the top bar

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function change() {
     document.body.style.background = "#272635";
     topBar.style.backgroundColor = "#A8D0DB";
     topBar.style.color = "#000000";
-    topBar.style.fontFamily = "Times New Roman, Times, serif"
+    topBar.style.fontFamily = "Nova Mono, monospace";
     option = true;
     gifler.innerHTML = "<h2 style='white-space:nowrap; font-size: clamp(1.70em, 1.75em, 1.75em);'>G I F F L A R </h2> Kan du godt lige giffler? hvis du kan så tryk lige her <button onclick='change()' style='background:none; border:none; margin:0; padding:0; cursor:pointer' class='a';><span id='aa'>👍🏿</span> <span id='ba'>👍</span></button></div>"
 }
